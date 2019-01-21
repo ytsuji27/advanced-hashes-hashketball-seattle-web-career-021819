58fcf3aa856af612ac9dbe868be1e66d850b0e5c
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-21 01:09:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -115,6 +115,7 @@ def num_points_scored(player)
   game_hash.each do |location, team_data|
     team_data[:players].each do |name, attribute|
       if name.to_s == player
+        return "Complete"
         points = attribute[:points]
       end
     end
